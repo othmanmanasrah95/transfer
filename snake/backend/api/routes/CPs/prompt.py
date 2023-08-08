f6b2e327a3335ebbf70e09a504866017e310db92
othmanmanasrah95 <othmanmanasrah95@gmail.com>
2023-08-08 16:13:33 +0200

    Imprving error handling

--- snake/backend/api/routes/CPs/prompt.py
+++
@@ -1,7 +0,0 @@
-import requests
-import sys
-
-x = " what is python "
-main_prompt_url = "http://localhost:5110/api/prompt_route"
-response = requests.post(main_prompt_url, data={"user_prompt": x})
-print(response.status_code)  # print HTTP response status code for debugging
--- snake/backend/app.js
+++ snake/backend/app.js
@@ -1,19 +1,21 @@
-
+// Require the necessary modules
 const express = require('express');
-const req = require('express/lib/request');
-const res = require('express/lib/response');
-const app = express();
-const contentRoutes = require('./api/routes/content');
-const promptRoutes = require('./api/routes/prompt');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+// Import route modules from local files
+const contentRoutes = require('./api/routes/content');
+const promptRoutes = require('./api/routes/prompt');
 
-const mongoose = require('mongoose');
+// Create an instance of the Express application
+const app = express();
 
-const url = 'mongodb://127.0.0.1:27017'; // Replace with your MongoDB connection string
-const dbName = 'llm'; // Replace with your database name
+// Define the URL and database name for MongoDB
+const url = 'mongodb://127.0.0.1:27017';
+const dbName = 'llm';
 
+// Connect to the MongoDB database using mongoose.connect
 mongoose.connect(`${url}/${dbName}`, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to the database');
@@ -23,49 +25,47 @@ mongoose.connect(`${url}/${dbName}`, { useNewUrlParser: true, useUnifiedTopology
     console.error('Failed to connect to the database:', err);
   });
 
-
-
+// Set up CORS handling using a middleware
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
-    if(req.method === 'OPTIONS'){
+    if (req.method === 'OPTIONS') {
         res.header("Access-Control-Allow-Methods", "GET, POST");
         return res.status(200).json({});
     }
-    next(); 
+    next();
 });
 
+// Set up logging using morgan middleware in "dev" mode
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: true}));
+
+// Set up body parsing using body-parser middleware to handle JSON and URL encoded data
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-//Routes
+// Define the routes for content and prompt using app.use, specifying the prefix for the routes
 app.use('/content', contentRoutes);
 app.use('/prompt', promptRoutes);
 
-
+// Define a middleware for handling "Not Found" errors
 app.use((req, res, next) => {
     const error = new Error('Not Found');
     error.status = 404;
     next(error);
 });
 
-
+// Define a global error handling middleware
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
-        error : {
-            message : error.message
+        error: {
+            message: error.message
         }
     });
 });
 
-
+// Export the Express application
 module.exports = app;
-
-
-
